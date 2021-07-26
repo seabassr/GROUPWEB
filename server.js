a653885408a805701adc6303235e97104cbd344d
seabassr <77996246+seabassr@users.noreply.github.com>
2021-07-26 15:00:06 -0500

    Stilling setting up

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const pagesRouter = require('./routes/pages');
+const mongoose = require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', pagesRouter);
+
+mongoose.connect('mongodb://localhost:27017/ShopDB', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(5000, () => {
+            console.log('MongoDB is connected and Express server is running...')
+        });
+    });

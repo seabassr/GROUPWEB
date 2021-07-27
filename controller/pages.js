0a6bb866c025a9f13a4094cd233171499b230ad7
seabassr <77996246+seabassr@users.noreply.github.com>
2021-07-27 00:35:03 -0500

    finish setup

--- controller/pages.js
+++ controller/pages.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const Contact = require('./models/contact');
-const Article = require('./models/article');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,14 +10,6 @@ router.get('/', (req, res) => {
 
 router.get('/contact', (req, res) => {
     res.render('contact');
-});
-
-router.get('/article', (req, res) => {
-    Article.find()
-        .then(results => {
-            res.render('article', {articles: results});
-        })
-        .catch(err => console.log(err));
 });
 
 router.post('/submitContact', (req, res) => {
@@ -33,29 +24,6 @@ router.post('/submitContact', (req, res) => {
     Contact.collection.insertOne(contact)
         .then(result => {
             res.render('submitContact');
-        })
-        .catch(err => console.log(err));
-});
-
-router.post('/submitArticle', (req, res) => {
-    const date = new Date().toLocaleString();
-
-    const article = new Article ({
-        title: req.body.title,
-        date: date,
-        content: req.body.content,
-        name: req.body.name
-    });
-
-    Article.collection.insertOne(article)
-        .then(result => {
-            console.log('result');
-        })
-        .catch(err => console.log(err));
-
-    Article.find()
-        .then(results => {
-            res.render('submitArticle', {articles: results});
         })
         .catch(err => console.log(err));
 });
